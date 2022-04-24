934788e4752a68e94bff4d3ded34a0e140724e34
Snehal1499 <snehalpatil1718@gmail.com>
2022-04-24 12:03:51 +0530

    Added Welcome Message

---
+++ IndianStatesCensusAnalyserProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianStatesCensusAnalyserProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Indian States Census Analyser Problem");
+        }
+    }
+}
